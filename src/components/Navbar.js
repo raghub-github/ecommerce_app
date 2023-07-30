3fdf6f2c1e537ff9c0534a17b20ad6453fa4359e
raghubhunia <raghubhunia53@gmail.com>
2023-07-30 17:27:14 +0530

    done

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import logo from "../image/pujari_logo.png"
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <img style={{ height: "5%", width: "5%", marginRight: "15px" }} src={logo} alt="Pujari logo" />
+                    {/* <Link className="navbar-brand" to="/"><strong>Pujari</strong></Link> */}
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            </li>
+
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Products
+                                </Link>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><Link className="dropdown-item" to="/">Rudraksha</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Books</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Dhuti</Link></li>
+                                    {/* <li><hr className="dropdown-divider" /></li> */}
+                                    <li><Link className="dropdown-item" to="/">Murti</Link></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">About</Link>
+                            </li>
+                            {/* <li className="nav-item">
+                                <Link className="nav-link disabled" to="/" tabindex="-1" aria-disabled="true">Disabled</Link>
+                            </li> */}
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
